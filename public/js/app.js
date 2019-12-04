5e2f2d2831dfd9f8aff4a41ebf824de38d95072d
Artis Gulbis <artux28@hotmail.com>
2019-12-04 20:59:12 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ if (weatherForm) {
 
         if (messageOne) messageOne.textContent = 'Loading...'
         if (messageTwo) messageTwo.textContent = ''
-        fetch(`http://192.168.178.27:3000/weather?address=${location}`).then((response) => {
+        fetch(`/weather?address=${location}`).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
                     messageOne.textContent = data.error
