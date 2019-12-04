aaf383dc86f9b35929a32184d6d5fffdca6054db
Artis Gulbis <artux28@hotmail.com>
2019-12-04 21:27:09 +0100

    set default location to Berlin

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,24 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const init = false;
+
+if (!init) {
+    if (messageOne) messageOne.textContent = 'Loading...'
+    if (messageTwo) messageTwo.textContent = ''
+    fetch(`/weather?address=Berlin`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                init = true;
+            }
+        })
+    })
+}
+
 
 if (weatherForm) {
     weatherForm.addEventListener('submit', (e) => {
